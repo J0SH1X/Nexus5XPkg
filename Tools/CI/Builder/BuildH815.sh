d926b2c32a4bd98cabcf88856931227f69594110
J0SH1X <aljoshua.hell@gmail.com>
2026-09-12 05:17:31 +0200

    Tools: BuildH815: use mkbootimg with --dt support

--- Tools/CI/Builder/BuildH815.sh
+++ Tools/CI/Builder/BuildH815.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Travis CI builder
 #
+# Copyright 2018, Bingxing Wang. <uefi-oss-projects@imbushuo.net>
 # Copyright 2026 Aljoshua Hell <aljoshua.hell@gmail.com>
 # All rights reserved.
 #
@@ -27,5 +28,5 @@ cat ./Nexus5XPkg/BootShim/BootShim.bin ./Build/LGG4-AARCH64/DEBUG_GCC5/FV/MSM899
 gzip -c < ./Build/LGG4-AARCH64/DEBUG_GCC5/FV/BootShimMSM8994_EFI.fd >./Build/LGG4-AARCH64/DEBUG_GCC5/FV/BootShimMSM8994_EFI.fd.gz
 
 # lge needs --dt support in mkbootimg.py
-python ./Nexus5XPkg/ImageResources/LGE/mkbootimg.py --kernel ./Build/LGG4-AARCH64/DEBUG_GCC5/FV/Image.gz-dtb --dt ./Nexus5XPkg/ImageResources/H815/msm8992-lge-h815-dt.img --ramdisk ./Nexus5XPkg/ImageResources/ramdisk-null --base 0x00000000 --pagesize 4096 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 -o ./Nexus5XPkg/ImageResources/H815/uefi_h815.img
+python ./Nexus5XPkg/ImageResources/LGE/mkbootimg/mkbootimg.py --kernel ./Build/LGG4-AARCH64/DEBUG_GCC5/FV/Image.gz-dtb --dt ./Nexus5XPkg/ImageResources/H815/msm8992-lge-h815-dt.img --ramdisk ./Nexus5XPkg/ImageResources/ramdisk-null --base 0x00000000 --pagesize 4096 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 -o ./Nexus5XPkg/ImageResources/H815/uefi_h815.img
 
